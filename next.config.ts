dc554e6ebc0616b9858900c13cde31257dd7ee1c
Santiago Mercanti <santi.mercanti15@gmail.com>
2025-12-02 00:35:00 -0300

    MQTT: fix nextconfig

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,7 @@ const basePath = (process.env.BASE_PATH || "").trim();
 const nextConfig: NextConfig = {
   // Si basePath es "", pasamos undefined para que Next no lo aplique en dev.
   basePath: basePath || undefined,
-    experimental: {
-    instrumentationHook: true,
-  },
+  
   // Hacer que los assets apunten a /a03/_next/...
   // Solo lo seteamos cuando hay basePath.
   assetPrefix: basePath ? `${basePath}/` : undefined,
